f18513071ad00c79ed68e4cc05b32e68a42ddf46
likemoses2025 <mose233@gmail.com>
2023-07-06 16:46:03 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <View className="bg-black flex-1">
+      <Text className="">App</Text>
+    </View>
+  );
+};
+
+export default App;
